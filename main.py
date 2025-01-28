00cedb1429b18e9627e7deefdbff2b2bc8954b1c
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-01-27 21:55:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import os
 import streamlit as st
-from crewai import Agent, Task, Process, Crew
-from langchain_openai import ChatOpenAI
 from datetime import datetime
-from etapas.mkt import planej_mkt_page
-from tools.retrieve import visualizar_planejamentos 
 from tavily import TavilyClient
 from etapas.osint import osint_report
 import google.generativeai as genai
